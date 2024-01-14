c935af63ea740920890c74d45eab0fe1143c9023
matt <mattkjames7@gmail.com>
2024-01-14 00:42:01 +0000

    new C++ test format

--- test/cpptest.cc
+++ test/cpptest.cc
@@ -1,9 +1,24 @@
-#include <stdio.h>
 #include <internalfield.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+void readTestData(double *Tx, double *Ty, double *Tz) {
+   	std::filesystem::path fileName = std::filesystem::current_path();
+    fileName /= "cpptest.bin";
+
+	std::ifstream file(fileName,std::ios::binary);
+
+	file.read(reinterpret_cast<char*>(Tx), sizeof(double));
+	file.read(reinterpret_cast<char*>(Ty), sizeof(double));
+	file.read(reinterpret_cast<char*>(Tz), sizeof(double));
+	file.close();
+}
+
 
 int main() {
 
-	printf("Testing C++\n");
+	std::cout << "C++ Test....................................";
 	
 	/* try getting a model object */
 	InternalModel model;
@@ -16,10 +31,27 @@ int main() {
 	double Bx, By, Bz;
 	model.Field(x,y,z,&Bx,&By,&Bz);
 
-	printf("B = [%6.1f,%6.1f,%6.1f] nT at [%4.1f,%4.1f,%4.1f]\n",Bx,By,Bz,x,y,z);
+	/* read in the saved data */
+	double Tx, Ty, Tz;
+	readTestData(&Tx, &Ty, &Tz);
 
-	printf("C++ test done\n");
-
+	/* compare results */
+	bool pass = (Tx == Bx) && (Ty == By) && (Tz == Bz);
+	if (pass) {
+		std::cout << "PASS" << std::endl;
+	} else {
+		std::cout << "FAIL" << std::endl;
+		std::cout << "Expected: ";
+		std::cout << "[" << std::setw(10) << std::setprecision(3) << std::fixed << Tx;
+		std::cout << ", " << std::setw(10) << std::setprecision(3) << std::fixed << Ty;
+		std::cout << ", " << std::setw(10) << std::setprecision(3) << std::fixed << Tz;
+		std::cout << "]" << std::endl;
+		std::cout << "Output: ";
+		std::cout << "[" << std::setw(10) << std::setprecision(3) << std::fixed << Bx;
+		std::cout << ", " << std::setw(10) << std::setprecision(3) << std::fixed << By;
+		std::cout << ", " << std::setw(10) << std::setprecision(3) << std::fixed << Bz;
+		std::cout << "]" << std::endl;
+	}
 
 }
 
