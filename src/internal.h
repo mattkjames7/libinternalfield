2d1e1f1272cc2e336c50546eacefb0012dc32e70
matt <mattkjames7@gmail.com>
2026-04-06 10:11:06 +0100

    update header

--- src/internal.h
+++ src/internal.h
@@ -11,12 +11,9 @@ namespace internalfield {
 namespace models {
 struct ModelView;
 }
-}
 
 
 /* This structure will store the Schmidt coefficients */
-namespace internalfield {
-
 struct schmidtcoeffs {
 	int n;
 	int m;
